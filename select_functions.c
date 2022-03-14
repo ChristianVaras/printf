8dee34373750fefac0addcbd4eb20ba545601443
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-14 23:42:52 +0000

    Function that creates an array of structures for the identifier

---
+++ select_functions.c
@@ -0,0 +1,17 @@
+/**
+ * select_functions - desc
+ *
+ * Return: counter
+ */
+int select_functions(void)
+{
+	f_type options[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_perc},
+		{"d", print_int},
+		{"i", print_int}
+	};
+
+	return (0);
+}
