e10a65c55106e70d392785920c111fb0b1295c00
ChristianVaras <christianvaras.2020@gamil.com>
2022-03-13 22:07:30 -0700

    Add the putchar and header files

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _printf - Simulates the printf function
+ * @format: Format to print to the console
+ * Return: number of char printed
+ */
+
+int _printf(const char *format, ...)
+{
+
+}
