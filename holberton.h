fa720f29c88a1f4fd284feb66a5a68aa9cb2a2ed
ChristianVaras <christianvaras.2020@gamil.com>
2022-03-14 17:50:43 -0700

    Adding function that prints char and string

--- holberton.h
+++ holberton.h
@@ -1,16 +1,28 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
 
-int _printf(const char *format, ...);
 /**
+ * struct Ftype - Define a structre for format
  *
- *
+ * @c: Parameter char(%c, %s, %i, ...)
+ * @f: Associated function
  */
 
 typedef struct Ftype
 {
 	char *c;
 	int(*f)(va_list);
-}f_type;
+}match_id;
+
+typedef struct Ftype match_id;
+
+/* main functions */
+int _printf(const char *format, ...);
+
+/* aux functions */
+int _putchar(char c);
 
 #endif
