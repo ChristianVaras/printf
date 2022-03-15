f4a944b89141f7d2a74fa7d01c8442f69e85355b
ChristianVaras <christianvaras.2020@gamil.com>
2022-03-14 22:10:25 -0700

    Add parser function

--- _printf.c
+++ _printf.c
@@ -1,14 +1,41 @@
 #include "holberton.h"
+int parser(const char *format, va_list arguments)
+{
+	char specifier;
+	int i, sum;
+	int printed_chars = 0;
 
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (*(format + i) == '%' || *(format + i) == '\\')
+		{
+			specifier = *(format + i + 1);
+			sum = select_functions(specifier, arguments);
+			if (sum == -1) /* no match was found */
+			{
+				_putchar(*(format + i));
+			}
+			else
+			{
+				printed_chars = printed_chars + sum;
+			}
+		}
+		else
+		{
+			_putchar(*(format + i));
+			printed_chars++;
+		}
+	}
+return (printed_chars);
+}
 /**
  * _printf - Simulates the printf function
  * @format: Format to print to the console
  * Return: number of char printed
  */
-
 int _printf(const char *format, ...)
 {
-	unsigned int final_count = 0;
+	int final_count = 0;
 	va_list arguments;
 
 	if(!format || arguments == NULL)
