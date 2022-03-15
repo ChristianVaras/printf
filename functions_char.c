9eef9a0b485ace62bd8d9a7f09d886f3069cd2eb
Mauricio Carrasco <vmcarrasco2810@gmail.com>
2022-03-15 23:20:39 +0000

    Fix betty and call new header file name

--- functions_char.c
+++ functions_char.c
@@ -1,35 +1,42 @@
-#include <holberton.h>
+#include "main.h"
 
 /**
- * print_char: print a char at printf
- * @list: the main string passed to the function
- * Return: An integer
+ * print_char - print a char at printf
+ * @arg: the main string passed to the function
+ *
+ * Return: 1
  */
-
 int print_char(va_list arg)
 {
 	char c;
+
 	c = va_arg(arg, int);
+
 	_putchar(c);
+
 	/* return true */
 	return (1);
 }
-/**
- *
- *
- */
 
+/**
+ * print_string - print a string at printf
+ * @arg: the main string passed to the function
+ *
+ * Return: The length of the string
+ */
 int print_string(va_list arg)
 {
 	int i = 0;
 	char *str;
 
 	str = va_arg(arg, char *);
+
 	if (str == NULL)
 	{
 		str == "(null)";
 	}
-	while ( str[i] != '\0')
+
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
