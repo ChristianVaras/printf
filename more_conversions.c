a299f0479cbef0ebc2eeb6aa1c811e33601c665f
ChristianVaras <christianvaras.2020@gamil.com>
2022-03-15 18:15:27 -0700

    Add format specifier and header statements

---
+++ more_conversions.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* print_rot13 - encode a string using rot13
+* @arg: string.
+* Return: string.
+*/
+int print_rot13(va_list arg)
+{
+	int i;
+	int x;
+	char *str;
+	char s[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char u[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (x = 0; x <= 52; x++)
+		{
+			if (str[i] == s[x])
+			{
+				_putchar(u[x]);
+				break;
+			}
+		}
+		if (x == 53)
+			_putchar(str[i]);
+	}
+	return (i);
+}
