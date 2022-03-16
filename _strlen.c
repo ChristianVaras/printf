49790404670509b6910d00843b2072d00d9083be
ChristianVaras <christianvaras.2020@gamil.com>
2022-03-15 19:47:35 -0700

    Add _strlen function and fix manpage

---
+++ _strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - returns the length of a string.
+* @s: parameter
+* Return: Always 0.
+**/
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
